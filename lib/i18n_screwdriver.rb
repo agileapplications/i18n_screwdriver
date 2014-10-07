ccdabd89ad676d3cab4c8d8fd30d6cf027147a68
Corin Langosch <info@corinlangosch.com>
2014-10-07 14:25:53 +0200

    Refactorings and minor fixes

--- lib/i18n_screwdriver.rb
+++ lib/i18n_screwdriver.rb
@@ -4,22 +4,27 @@ require "i18n_screwdriver/translation_helper"
 require "i18n_screwdriver/rails"
 
 module I18nScrewdriver
-  def self.filename(locale)
+  def self.filename_for_locale(locale)
     File.join("config", "locales", "application.#{locale}.yml")
   end
 
   def self.for_key(string)
-    Digest::MD5.hexdigest(string.strip)
+    string = string.strip
+    (string =~ /^:[a-z][a-z0-9_]*$/) ? string : Digest::MD5.hexdigest(string)
+  end
+
+  def self.file_with_translations_exists?(locale)
+    File.exists?(filename_for_locale(locale))
   end
 
   def self.load_translations(locale)
-    path = filename(locale)
+    path = filename_for_locale(locale)
     raise "File #{path} not found!" unless File.exists?(path)
-    YAML.load_file(path)[locale]
+    sanitize_hash(YAML.load_file(path)[locale])
   end
 
   def self.write_translations(locale, translations)
-    File.open(filename(locale), "w") do |file|
+    File.open(filename_for_locale(locale), "w") do |file|
       file.puts "# use rake task i18n:update to generate this file"
       file.puts
       file.puts({locale => in_utf8(translations)}.to_yaml)
@@ -35,7 +40,7 @@ module I18nScrewdriver
   end
 
   def self.grab_symbols_to_be_translated(string)
-    string.scan(/_\(:(.*?)\)/).flatten
+    string.scan(/_\((:[a-z][a-z0-9_]*)\)/).flatten
   end
 
   def self.in_utf8(hash)
@@ -80,14 +85,13 @@ module I18nScrewdriver
   def self.gather_translations
     texts = []
     symbols = []
-    Dir.glob(folder).each do |file|
+    Dir.glob("**/*.{haml,erb,slim,rb}").each do |file|
       input = File.read(file)
       texts.concat(grab_texts_to_be_translated(input))
       symbols.concat(grab_symbols_to_be_translated(input))
     end
-
     translations = Hash[texts.uniq.map{ |text| [for_key(text), text] }]
-    translations.merge(Hash[symbols.uniq.map{ |symbol| [symbol, ""] }])
+    translations.merge(Hash[symbols.uniq.map{ |symbol| [for_key(symbol), ""] }])
   end
 
   def self.default_locale
@@ -107,5 +111,22 @@ module I18nScrewdriver
   def self.dummy_text
     "TRANSLATION_MISSING"
   end
-end
 
+  def self.update_translations_file(locale, translations)
+    existing_translations = file_with_translations_exists?(locale) ? load_translations(locale) : {}
+    existing_translations.select!{ |k| translations.has_key?(k) }
+
+    translations.each do |k, v|
+      next if existing_translations[k]
+      existing_translations[k] = (default_locale == locale) ? v : dummy_text
+    end
+
+    write_translations(locale, existing_translations)
+  end
+
+  def self.sanitize_hash(hash)
+    {}.tap do |new_hash|
+      hash.each{ |k, v| new_hash[k.to_s] = v.to_s }
+    end
+  end
+end
--- lib/tasks/i18n.rake
+++ lib/tasks/i18n.rake
@@ -1,31 +1,10 @@
 namespace :i18n do
   desc 'Find all translations in views, helpers and controllers'
   task :update => :environment do
-    translations = I18nScrewdriver.gather_translations("**/*.{haml,erb,slim,rb}")
-    I18nScrewdriver.write_translations(default_locale, translations)
+    translations = I18nScrewdriver.gather_translations
     puts "Found #{translations.keys.size} unique translations"
-  end
-
-  desc 'Write dummy translation files so they can manually be translated'
-  task :update_foreign => :environment do
-    # load translations from default locale
-    translations = I18nScrewdriver.load_translations(default_locale)
-
-    # process each language we'd like to translate
-    (available_locales - [default_locale]).each do |locale|
-      filename = I18nScrewdriver.filename(locale)
-
-      # load existing translations
-      existing_translations = File.exists?(filename) ? I18nScrewdriver.load_translations(locale) : {}
-
-      # delete removed translations from hash
-      existing_translations.reject!{ |k| !translations.has_key?(k) }
-
-      # add new translations
-      translations.each_key{ |k| existing_translations[k] ||= dummy_text }
-
-      # write the file
-      I18nScrewdriver.write_translations(locale, existing_translations)
+    I18nScrewdriver.available_locales.each do |locale|
+      I18nScrewdriver.update_translations_file(locale, translations)
     end
   end
 
