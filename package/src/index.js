1f4e86435a3f2cccffba50680954e97323fe7b71
Manuel Bouza <manuel@bouza.ch>
2020-05-06 17:05:32 +0200

    Define screw method directly on I18n

--- package/src/index.js
+++ package/src/index.js
@@ -8,13 +8,11 @@ function onMissingTranslation(md5, message) {
 const interpolate = (message, data) =>
   message.replace(/%{([^{}]*)}/g, (a, b) => data[b]);
 
-export default function configure(I18n) {
-  I18n.screw = function screw(message, data) {
-    const hash = md5(message);
-    let translation = I18n.translations[I18n.locale]?.[hash];
-    if (typeof translation === "undefined") {
-      translation = onMissingTranslation(hash, message);
-    }
-    return interpolate(translation, data);
-  };
-}
+I18n.screw = function screw(message, data) {
+  const hash = md5(message);
+  let translation = I18n.translations[I18n.locale]?.[hash];
+  if (typeof translation === "undefined") {
+    translation = onMissingTranslation(hash, message);
+  }
+  return interpolate(translation, data);
+};
