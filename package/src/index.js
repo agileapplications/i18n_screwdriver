30e479fc67eea725b25be8a56644d9d4d15651a5
Manuel Bouza <manuel@bouza.ch>
2020-05-06 17:36:56 +0200

    Use configure

--- package/src/index.js
+++ package/src/index.js
@@ -1,18 +1,19 @@
 import md5 from "md5";
-import I18n from "i18n-js";
 
-function onMissingTranslation(md5, message) {
+function onMissingTranslation(I18n, md5, message) {
   return `translation missing: ${I18n.locale}.${message}`;
 }
 
 const interpolate = (message, data) =>
   message.replace(/%{([^{}]*)}/g, (a, b) => data[b]);
 
-I18n.screw = function screw(message, data) {
-  const hash = md5(message);
-  let translation = I18n.translations[I18n.locale]?.[hash];
-  if (typeof translation === "undefined") {
-    translation = onMissingTranslation(hash, message);
-  }
-  return interpolate(translation, data);
-};
+export default function configure(I18n) {
+  I18n.screw = function screw(message, data) {
+    const hash = md5(message);
+    let translation = I18n.translations[I18n.locale]?.[hash];
+    if (typeof translation === "undefined") {
+      translation = onMissingTranslation(I18n, hash, message);
+    }
+    return interpolate(translation, data);
+  };
+}
