6f61c0c3ae0a7091f955b7814ab4c3dd458d0568
Joakim Repomaa <joakim.repomaa@mocoapp.com>
2021-12-01 10:17:56 +0200

    add support for interpolating links

--- app/assets/javascrips/i18n_screwdriver/screw.js
+++ app/assets/javascrips/i18n_screwdriver/screw.js
@@ -1,13 +1,16 @@
 //= require i18n_screwdriver/md5
 
-const interpolate = (message, data) =>
-  message.replace(/%{([^{}]*)}/g, (a, b) => data[b]);
+const interpolate = (message, data, links = []) =>
+  links.reduce(
+    (result, href) => result.replace(/<<(.*?)>>/, `<a href="${href}">$1</a>`),
+    message.replace(/%{([^{}]*)}/g, (_, b) => data[b])
+  );
 
-I18n.screw = function(message, data) {
+I18n.screw = function (message, data, links = []) {
   const md5 = window.md5(message);
   let translation = I18n.translations[I18n.locale][md5];
   if (translation == null) {
     translation = I18n.onMissingTranslation(md5, message);
   }
-  return interpolate(translation, data);
+  return interpolate(translation, data, links);
 };
--- package/src/index.js
+++ package/src/index.js
@@ -4,16 +4,19 @@ function onMissingTranslation(I18n, md5, message) {
   return `translation missing: ${I18n.locale}.${message}`;
 }
 
-const interpolate = (message, data) =>
-  message.replace(/%{([^{}]*)}/g, (a, b) => data[b]);
+const interpolate = (message, data, links = []) =>
+  links.reduce(
+    (result, href) => result.replace(/<<(.*?)>>/, `<a href="${href}">$1</a>`),
+    message.replace(/%{([^{}]*)}/g, (_, b) => data[b])
+  );
 
 export default function configure(I18n) {
-  I18n.screw = function screw(message, data) {
+  I18n.screw = function screw(message, data, links = []) {
     const hash = md5(message);
     let translation = I18n.translations[I18n.locale]?.[hash];
     if (typeof translation === "undefined") {
       translation = onMissingTranslation(I18n, hash, message);
     }
-    return interpolate(translation, data);
+    return interpolate(translation, data, links);
   };
 }
