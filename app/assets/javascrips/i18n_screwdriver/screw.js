c4b504c0c8a75deb746dff4984ec2c612d8dbb6a
Joakim Repomaa <joakim.repomaa@mocoapp.com>
2021-12-17 20:11:49 +0200

    use guard clause

--- app/assets/javascrips/i18n_screwdriver/screw.js
+++ app/assets/javascrips/i18n_screwdriver/screw.js
@@ -3,30 +3,30 @@
 const interpolate = (message, data, links, render) => {
   const interpolated = message.replace(/%{([^{}]*)}/g, (_, b) => data[b]);
 
-  if (typeof render === "function") {
-    const parts = interpolated.match(/<<.*?>>|.+?(?=<<.*?>>|$)/g) || [];
-
-    let linkIndex = 0;
-
-    return parts.map((part, index) => {
-      if (part.startsWith("<<") && part.endsWith(">>")) {
-        const href = links[linkIndex];
-        if (typeof href === "undefined") {
-          throw new Error("More links in template string than hrefs");
-        }
-
-        linkIndex++;
-        return render({ text: part.slice(2, -2), href, index });
-      }
-
-      return render({ text: part, index });
-    });
+  if (typeof render !== "function") {
+    return links.reduce(
+      (result, href) => result.replace(/<<(.*?)>>/, `<a href="${href}">$1</a>`),
+      interpolated
+    );
   }
 
-  return links.reduce(
-    (result, href) => result.replace(/<<(.*?)>>/, `<a href="${href}">$1</a>`),
-    interpolated
-  );
+  const parts = interpolated.match(/<<.*?>>|.+?(?=<<.*?>>|$)/g) || [];
+
+  let linkIndex = 0;
+
+  return parts.map((part, index) => {
+    if (part.startsWith("<<") && part.endsWith(">>")) {
+      const href = links[linkIndex];
+      if (typeof href === "undefined") {
+        throw new Error("More links in template string than hrefs");
+      }
+
+      linkIndex++;
+      return render({ text: part.slice(2, -2), href, index });
+    }
+
+    return render({ text: part, index });
+  });
 };
 
 I18n.screw = function screw(message, data, { links = [], render } = {}) {
--- package/src/index.js
+++ package/src/index.js
@@ -7,30 +7,30 @@ function onMissingTranslation(I18n, md5, message) {
 const interpolate = (message, data, links, render) => {
   const interpolated = message.replace(/%{([^{}]*)}/g, (_, b) => data[b]);
 
-  if (typeof render === "function") {
-    const parts = interpolated.match(/<<.*?>>|.+?(?=<<.*?>>|$)/g) || [];
-
-    let linkIndex = 0;
-
-    return parts.map((part, index) => {
-      if (part.startsWith("<<") && part.endsWith(">>")) {
-        const href = links[linkIndex];
-        if (typeof href === "undefined") {
-          throw new Error("More links in template string than hrefs");
-        }
-
-        linkIndex++;
-        return render({ text: part.slice(2, -2), href, index });
-      }
-
-      return render({ text: part, index });
-    });
+  if (typeof render !== "function") {
+    return links.reduce(
+      (result, href) => result.replace(/<<(.*?)>>/, `<a href="${href}">$1</a>`),
+      interpolated
+    );
   }
 
-  return links.reduce(
-    (result, href) => result.replace(/<<(.*?)>>/, `<a href="${href}">$1</a>`),
-    interpolated
-  );
+  const parts = interpolated.match(/<<.*?>>|.+?(?=<<.*?>>|$)/g) || [];
+
+  let linkIndex = 0;
+
+  return parts.map((part, index) => {
+    if (part.startsWith("<<") && part.endsWith(">>")) {
+      const href = links[linkIndex];
+      if (typeof href === "undefined") {
+        throw new Error("More links in template string than hrefs");
+      }
+
+      linkIndex++;
+      return render({ text: part.slice(2, -2), href, index });
+    }
+
+    return render({ text: part, index });
+  });
 };
 
 export default function configure(I18n) {
